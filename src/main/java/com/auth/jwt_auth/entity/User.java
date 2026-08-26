afb3183470dbb691d77ef18a1efece0c5ebd9037
bhaveshkhose <khosebhavesh@gmail.com>
2026-08-26 10:51:41 +0530

    Configure the database module

---
+++ src/main/java/com/auth/jwt_auth/entity/User.java
@@ -0,0 +1,26 @@
+package com.auth.jwt_auth.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id ;
+
+    @Column(unique = true)
+    private String username ;
+
+    private String password ;
+}
---
+++ src/main/java/com/auth/jwt_auth/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.auth.jwt_auth.repository;
+
+import com.auth.jwt_auth.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+  User findByUsername(String username);
+
+  boolean existsByUsername(String username);
+
+}
